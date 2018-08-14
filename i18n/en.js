8b3d6c7a2c9ebb6600cc0ba23637507c0de4d958
knox <knox.h@qq.com>
2018-08-14 14:37:27 +0800

    add qqplayCore update tips for fireball/issues/8139

---
+++ i18n/en.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let dialog = {
+    message: 'Update tips',
+    detail: 'Check to qqPlayCore for a new version, update to the current project build-templates?',
+};
+
+let log = {
+    complete: 'Update qqPlayCore completed, saved in %{path}'
+};
+
+
+module.exports = {
+    dialog
+};
