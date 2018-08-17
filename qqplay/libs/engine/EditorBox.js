af406b0d052c78b5e2b839629fdf6f9f7efa4c92
knox <knox.h@qq.com>
2018-08-17 14:35:55 +0800

    fix editorbox bug in android for fireball/issues/8186

--- qqplay/libs/engine/EditorBox.js
+++ qqplay/libs/engine/EditorBox.js
@@ -1,67 +1,67 @@
 var KeyboardReturnType = cc.EditBox.KeyboardReturnType;
 var _p = cc.EditBox._EditBoxImpl.prototype;
 
-var curEditBoxComp;
-var curEditBoxImpl;
+var component;
+var impl;
 
 function _onBKBtnClick (text) {
-    this._text = text;
-    if (this._delegate && this._delegate.editBoxEditingReturn) {
-        this._delegate.editBoxEditingReturn();
+
+    _onBKTextChange(text);
+
+    if (impl._delegate && impl._delegate.editBoxEditingReturn) {
+        impl._delegate.editBoxEditingReturn();
     }
-    if (this._delegate && this._delegate.editBoxEditingDidEnded) {
-        this._delegate.editBoxEditingDidEnded();
+    if (impl._delegate && impl._delegate.editBoxEditingDidEnded) {
+        impl._delegate.editBoxEditingDidEnded();
     }
-    this._editing = false;
+
+    impl._editing = false;
+    BK.Editor.hideKeyBoard();
 }
 
 function _onBKTextChange (text) {
-    if (text.length > this._maxLength) {
-        text = text.slice(0, this._maxLength);
+    if (text.length > impl._maxLength) {
+        text = text.slice(0, impl._maxLength);
     }
 
-    if (this._delegate && this._delegate.editBoxTextChanged) {
-        if (this._text !== text) {
-
+    if (impl._delegate && impl._delegate.editBoxTextChanged) {
+        if (impl._text !== text) {
             if (text === '') {
                 _hideTextLabel();
             }
             else {
                 _showTextLabel();
             }
-
-            this._text = text;
-            this._delegate.editBoxTextChanged(this._text);
+            impl._text = text;
+            impl._delegate.editBoxTextChanged(impl._text);
         }
     }
 }
 
 function _showTextLabel () {
-    if (curEditBoxComp) {
-        curEditBoxComp._textLabel.node.active = true;
-        curEditBoxComp._placeholderLabel.node.active = false;
+    if (component) {
+        component._textLabel.node.active = true;
+        component._placeholderLabel.node.active = false;
     }
 }
 
 function _hideTextLabel () {
-    if (curEditBoxComp) {
-        curEditBoxComp._textLabel.node.active = false;
-        curEditBoxComp._placeholderLabel.node.active = true;
+    if (component) {
+        component._textLabel.node.active = false;
+        component._placeholderLabel.node.active = true;
     }
 }
 
 _p._beginEditing = function () {
-    if (curEditBoxImpl !== this) {
-        curEditBoxImpl = this;
-        curEditBoxComp = this._node.getComponent(cc.EditBox);
+    if (impl !== this) {
+        impl = this;
+        component = this._node.getComponent(cc.EditBox);
     }
-    BK.Editor.showKeyBoard(_onBKBtnClick.bind(this), _onBKTextChange.bind(this));
-    BK.Editor.setText(this._text);
-    if (this._delegate && this._delegate.editBoxEditingDidBegan) {
-        this._delegate.editBoxEditingDidBegan();
+    BK.Editor.showKeyBoard(_onBKBtnClick.bind(impl), _onBKTextChange.bind(impl));
+    BK.Editor.setText(impl._text);
+    if (impl._delegate && impl._delegate.editBoxEditingDidBegan) {
+        impl._delegate.editBoxEditingDidBegan();
     }
-
     _showTextLabel();
-
-    this._editing = true;
+    impl._editing = true;
 };
