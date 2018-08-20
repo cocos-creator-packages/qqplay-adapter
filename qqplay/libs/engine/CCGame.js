377eb862a2c4950299ee69c62f3c28a5807ec60a
knox <knox.h@qq.com>
2018-08-20 14:33:43 +0800

    supports setFrameRate

--- qqplay/libs/engine/CCGame.js
+++ qqplay/libs/engine/CCGame.js
@@ -7,3 +7,24 @@ BK.Notification.on("game_enter_background", {}, function () {
 BK.Notification.on("game_enter_foreground", {}, function () {
     cc.game.emit(cc.game.EVENT_SHOW);
 });
+
+
+//
+var _frameRate = 60;
+cc.game.setFrameRate = function (frameRate) {
+    _frameRate = frameRate;
+    if (_frameRate === 60) {
+        BK.Director.tickerSetInterval(1);
+    }
+    else if (_frameRate === 30) {
+        BK.Director.tickerSetInterval(2);
+    }
+    else {
+        BK.Script.log(1, 0, '-- only supports frame rate of 60 or 30 on qqplay --');
+        BK.Director.tickerSetInterval(1);
+    }
+};
+
+cc.game.getFrameRate = function () {
+    return _frameRate;
+};
