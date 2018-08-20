6b43f042bce8d5680850a4ffe0101af1ae6544be
knox <knox.h@qq.com>
2018-08-20 15:17:21 +0800

    adapter _runMainLoop

--- qqplay/libs/engine/CCGame.js
+++ qqplay/libs/engine/CCGame.js
@@ -28,3 +28,21 @@ cc.game.setFrameRate = function (frameRate) {
 cc.game.getFrameRate = function () {
     return _frameRate;
 };
+
+cc.game._runMainLoop = function () {
+    var self = this, callback, config = self.config,
+        director = cc.director,
+        skip = true, frameRate = config.frameRate;
+
+    cc.debug.setDisplayStats(config.showFPS);
+
+    callback = function () {
+        if (!self._paused) {
+            self._intervalId = window.requestAnimFrame(callback);
+            director.mainLoop();
+        }
+    };
+
+    self._intervalId = window.requestAnimFrame(callback);
+    self._paused = false;
+};
