f9dbe925f192e5b473c4ed92dc32b7e6415ec6b6
knox <knox.h@qq.com>
2018-11-02 17:48:51 +0800

    add element/index.js

---
+++ qqplay/libs/element/index.js
@@ -0,0 +1,10 @@
+// 用于适配 element 层
+BK.Script.loadlib('GameRes://libs/element/Utils.js');
+BK.Script.loadlib('GameRes://libs/element/HTMLElemenet.js');
+BK.Script.loadlib('GameRes://libs/element/HTMLAudioElement.js');
+BK.Script.loadlib('GameRes://libs/element/HTMLImageElement.js');
+BK.Script.loadlib('GameRes://libs/element/HTMLScriptElement.js');
+BK.Script.loadlib('GameRes://libs/element/HTMLVideoElement.js');
+BK.Script.loadlib('GameRes://libs/element/Document.js');
+BK.Script.loadlib('GameRes://libs/element/Canvas.js');
+BK.Script.loadlib('GameRes://libs/element/XMLHttpRequest.js');
